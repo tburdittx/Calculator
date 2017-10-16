6a5b8ad77e0fa57bdf0d27c3b9a5648e24800533
Tara <tburdittx@live.com>
2017-10-16 09:36:37 +0100

    added multiplier function

---
+++ calculator3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator");
+
+            Console.WriteLine("Enter your first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+          
+            int answer = firstNumber * secondNumber;
+
+            Console.WriteLine(answer);            
+
+            Console.ReadLine();
+
+        }
+    }
+}
