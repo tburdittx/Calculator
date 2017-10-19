3dae2b44ddf58250d992945061524bd279d6aaef
Tara <tburdittx@live.com>
2017-10-19 09:10:20 +0100

    Added more operators

--- calculator3.cs
+++ calculator3.cs
@@ -12,15 +12,39 @@ namespace Calculator2
         {
             Console.WriteLine("Welcome to the calculator");
 
+            Console.WriteLine("Enter your operator");
+            string operation = Console.ReadLine();
+
             Console.WriteLine("Enter your first number");
             int firstNumber = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter your second number");
             int secondNumber = int.Parse(Console.ReadLine());
-          
-            int answer = firstNumber * secondNumber;
 
-            Console.WriteLine(answer);            
+
+            int answer = 0;
+
+            if (operation == "+")
+            {
+                answer = firstNumber + secondNumber;
+            }
+
+            if (operation == "*")
+            {
+                answer = firstNumber * secondNumber;
+            }
+
+            if (operation == "-")
+            {
+                answer = firstNumber - secondNumber;
+            }
+
+            if (operation == "/")
+            {
+                answer = firstNumber / secondNumber;
+            }
+           
+            Console.WriteLine("The answer is: " + answer);
 
             Console.ReadLine();
 
