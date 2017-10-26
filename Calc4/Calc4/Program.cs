dd52888332def0ee6b38dd5bd158a1eff31f3403
Tara <tburdittx@live.com>
2017-10-26 15:59:26 +0100

    Added methods and loops

---
+++ Calc4/Calc4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintWelcomeMessage();
+
+            while (true)
+            {
+                PerformACalculation();
+            }
+        }
+
+        public static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Calculator");
+        }
+
+        public static void PerformACalculation()
+        {
+            Console.WriteLine("Enter your operator");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("How many numbers do you want to " + operation);
+            int UserInputOfHowMany = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[UserInputOfHowMany];
+            for (int i = 0; i < UserInputOfHowMany; i++)
+            {
+                Console.WriteLine("Please enter number " + (i + 1) + ": ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            int answer = 0;
+
+            for (int i = 0; i < UserInputOfHowMany; i++)
+            {
+                if (operation == "+")
+                {
+                    answer = answer + numbers[i];
+                }
+
+                if (operation == "*")
+                {
+                    answer = answer * numbers[i];
+                }
+
+                if (operation == "-")
+                {
+                    answer = answer - numbers[i];
+                }
+
+                if (operation == "/")
+                {
+                    answer = answer / numbers[i];
+                }
+            }
+            Console.WriteLine("This is the answer: " + answer);
+            Console.ReadLine();
+        }
+    }
+}
