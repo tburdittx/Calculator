8187df5fd7131e45f1900ff4410cb2e9d028bbc1
Tara <tburdittx@live.com>
2017-10-27 11:45:00 +0100

    Added in a date function

--- Calc4/Calc4/Program.cs
+++ Calc4/Calc4/Program.cs
@@ -8,14 +8,40 @@ namespace Calculator2
 {
     class Program
     {
+        private const int NumberCalculator = 1;
+        private const int DateCalculator = 2;
         static void Main(string[] args)
         {
             PrintWelcomeMessage();
 
             while (true)
             {
-                PerformACalculation();
+                int calculationMode = AskForCalculationMode();
+                if (calculationMode == NumberCalculator)
+                {
+                    PerformACalculation();
+                }
+                else
+                {
+                    CalculatingDate();
+                }
             }
+        }
+        private static int AskForCalculationMode()
+        {
+            Console.WriteLine("Which calculator mode do you want?");
+            Console.WriteLine(" {0}) Numbers", NumberCalculator);
+            Console.WriteLine(" {0}) Dates", DateCalculator);
+            return AskForNumber("> ");
+        }
+
+        public static int AskForNumber(string message)
+        {
+            string message1 = "Please enter a number";
+            Console.WriteLine(message1);
+            int numbergiven = new int();
+            numbergiven = int.Parse(Console.ReadLine());
+            return numbergiven;
         }
 
         public static void PrintWelcomeMessage()
@@ -63,6 +89,41 @@ namespace Calculator2
             }
             Console.WriteLine("This is the answer: " + answer);
             Console.ReadLine();
+
+        }
+        public static void CalculatingDate()
+        {
+            var date = AskForDate("Please enter a date in the form MM/DD/YYYY: ");
+            var number = AskForNumber2("Please enter the number of days to add: ");
+            var answer = date.AddDays(number);
+
+            Console.WriteLine("The answer is: {0:dd/MM/yyyy}", answer);
+            Console.WriteLine();
+
+        }
+
+        private static DateTime AskForDate(string message)
+        {
+            DateTime date;
+
+            do
+            {
+                Console.Write(message);
+            } while (!DateTime.TryParse(Console.ReadLine(), out date));
+
+            return date;
+        }
+        private static int AskForNumber2(string message)
+        {
+            int count;
+
+            do
+            {
+                Console.Write(message);
+            } while (!int.TryParse(Console.ReadLine(), out count));
+
+            return count;
         }
     }
 }
+
