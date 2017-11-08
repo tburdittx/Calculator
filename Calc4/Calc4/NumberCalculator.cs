3a0e1ccea210c4b29f925d5113da1513e6b85b54
Tara <tburdittx@live.com>
2017-11-08 08:56:40 +0000

    Added enum and switch statement

--- Calc4/Calc4/NumberCalculator.cs
+++ Calc4/Calc4/NumberCalculator.cs
@@ -33,12 +33,20 @@ namespace Calc4
             this.logCalculator = logCalculator;
         }
 
+        enum Operation
+        {
+           Plus,
+            Minus,
+            Multiply,
+            Divide
+        }
         public void ListOfNumbers5()
         {
             Console.WriteLine("Enter your operator");
-            string operation = Console.ReadLine();
-            List<int> numbersInput = new List<int>();
+           string operation = Console.ReadLine();
+       
 
+            List<int> numbersInput = new List<int>();
             Console.WriteLine("Please enter your number");
             string input = Console.ReadLine();
             numbersInput.Add(int.Parse(input));
@@ -47,8 +55,7 @@ namespace Calc4
             {
                 Console.WriteLine("Please enter your next number: ");
                 input = Console.ReadLine();
-                int value;
-                if (!int.TryParse(input, out value))
+                if (!int.TryParse(input, out int value))
                 {
                     //error
                 }
@@ -60,27 +67,25 @@ namespace Calc4
 
             int answer = 0;
 
-            if (operation == "*")
+            switch (operation)
             {
-                answer = numbersInput.Aggregate(1, (acc, number) => acc *number);
+                case "+":
+                    answer = numbersInput.Sum();
+                    break;
+                case "-":
+                    answer = numbersInput.Skip(1).Aggregate(numbersInput[0], (acc, number) => acc - number);
+                    break;
+                case "*":
+                    answer = numbersInput.Aggregate(1, (acc, number) => acc * number);
+                    break;
+                case "/":
+                    answer = numbersInput.Skip(1).Aggregate(numbersInput[0], (acc, number) => acc / number);
+                    break;
+                default: Console.WriteLine("Could not apply operator");
+                    break;
             }
-            else if (operation == "/")
-            {
-                answer = numbersInput.Skip(1).Aggregate(numbersInput[0], (acc, number) => acc / number);
-            }
-            else if (operation == "+")
-            {
-                answer = numbersInput.Sum();
-            }
-
-            else if (operation == "-")
-            {
-                answer = numbersInput.Skip(1).Aggregate(numbersInput[0], (acc, number) => acc - number);
-            }
-
             Console.WriteLine(answer);
         }
-
         private void PerformACalculation()
         {
             ListOfNumbers5();
